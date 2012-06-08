b9ee8e6d589b6c317851bfc099330a219b983a81
esehara <esehara@gmail.com>
2012-06-08 22:29:21 +0900

    [Add] server.js

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ function render_index(res,post_id,formval,page) {
 };
 
 
-app.listen(8080);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 //Socket.io
